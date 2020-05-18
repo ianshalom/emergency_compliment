c4f011839b94fe251706bb27920f83fd3527e459
“Marcelchia” <marcel.chiayl@gmail.com>
2020-05-18 12:19:18 +0800

    add length

--- app/models/fruit.rb
+++ app/models/fruit.rb
@@ -1,5 +1,12 @@
 class Fruit < ApplicationRecord
-  def standard_weight
-    weight * 2.2
-  end
+    def standard_weight
+      weight * 2.2
+    end
+
+    def standard_length
+      length * 50
+    end
+    def hello
+      puts "hello"
+    end
 end
